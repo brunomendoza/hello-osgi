b09a8409eb5b5a6879b1955e48a57ab4f5214595
Bruno Mendoza <hola@brunomendoza.es>
2021-09-25 15:08:57 +0100

    Service and consumer a prepared for interacting with Karaf

---
+++ hello-osgi-consumer/src/main/java/es/brunomendoza/lab/osgi/consumer/Consumer.java
@@ -0,0 +1,53 @@
+package es.brunomendoza.lab.osgi.consumer;
+
+import es.brunomendoza.lab.osgi.service.IGreeter;
+import org.osgi.framework.*;
+
+public class Consumer implements BundleActivator, ServiceListener {
+
+    private BundleContext bundleContext;
+    private ServiceReference serviceReference;
+
+    // The addServiceListener() method allows the client to ask the platform
+    // to send notifications about the service that complies with the provided expression.
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        this.bundleContext = bundleContext;
+        try {
+            bundleContext.addServiceListener(
+                    this,
+                    String.format("(objectclass=%$)", IGreeter.class.getName())
+            );
+        } catch (InvalidSyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        if (serviceReference != null) {
+            bundleContext.ungetService(serviceReference);
+        }
+    }
+
+    @Override
+    public void serviceChanged(ServiceEvent serviceEvent) {
+        int type = serviceEvent.getType();
+
+        switch (type) {
+            case(ServiceEvent.REGISTERED):
+                System.out.println("Notificadtion of service registered.");
+                serviceReference = serviceEvent
+                        .getServiceReference();
+                IGreeter service = (IGreeter) (bundleContext.getService(serviceReference));
+                System.out.println(service.sayHiTo("John"));
+                break;
+            case(ServiceEvent.UNREGISTERING):
+                System.out.println("Notification of service unregistered");
+                bundleContext.ungetService(serviceEvent.getServiceReference());
+                break;
+            default:
+                break;
+        }
+    }
+}
