c8581e341e6db180554763aceeb4cd6bacf2be9d
Bruno Mendoza <hola@brunomendoza.es>
2021-09-25 11:44:49 +0100

    Creating a bundle entry point

---
+++ hello-osgi-service/src/main/java/App.java
@@ -0,0 +1,14 @@
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class App implements BundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("Hello, Bundle!");
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        System.out.printf("Bye, Bundle");
+    }
+}
